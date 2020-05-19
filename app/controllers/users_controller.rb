b5cbd81975efbefde5eb7d8cc6f64d6d7205b5eb
therealjustindude <therealjustindude@gmail.com>
2020-05-19 11:00:50 -0500

    get and post /signup routes completed for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,22 @@ class UsersController < ApplicationController
     end
 
     get '/signup' do
-        
+        if !logged_in?
+            erb :'/users/create_user'        
+        else
+            redirect '/show'
+        end
     end
 
     post '/signup' do
-        
+        if params[:name] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            @user= User.new(name: params[:name], email: params[:email], password: params[:password])
+            @user.save
+            session[:user_id] = @user.id
+            redirect '/show'
+        end
     end
 
     get '/logout' do
