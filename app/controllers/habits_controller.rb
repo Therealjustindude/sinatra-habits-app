c10f389d7c3908f059a182c42e11239e98f68c7d
therealjustindude <therealjustindude@gmail.com>
2020-05-19 16:55:18 -0500

    assign a current users habits to @habits

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -2,14 +2,14 @@ class HabitsController < ApplicationController
    #read
     get '/habits' do
         if logged_in?
-            @habits = Habits.all
+            @habits = current_user.habits.all
             erb :"habits/habits_index"            
         else
             redirect "/login"
         end
     end
 
-    get '/habits/:id'
+    get '/habits/:id' do
         if logged_in?
             @habit = Habit.find_by(id: params[:id])
             erb :'/habits/show_habit'
