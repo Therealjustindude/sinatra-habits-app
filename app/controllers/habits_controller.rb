f358cc2a8f7489eba6e9908a2a3a2ed743312af2
therealjustindude <therealjustindude@gmail.com>
2020-05-19 15:56:21 -0500

    work on get routes for HabitsController

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,3 +1,10 @@
 class HabitsController < ApplicationController
-    
+   #read
+    get '/habits' do
+        erb :"habits/habits_index"
+    end
+
+    get '/habits/:id'
+        redirect '/habits/show_habit'
+    end
 end
