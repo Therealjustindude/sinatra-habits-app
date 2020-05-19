998f29d4dedd15f9148c6cd03f3c1504ccf46677
therealjustindude <therealjustindude@gmail.com>
2020-05-19 00:47:19 -0500

    added login and signup hrefs in index.erb. Added login and signup routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 require './config/environment'
-
-
 class ApplicationController < Sinatra::Base
 
   configure do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,36 @@
 class UsersController < ApplicationController
     
-    get '/users' do
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'/users/show'
+    end
+
+    get '/login' do
         erb :'/users/login'
     end
 
-    post '/users' do
+    post '/login' do
+        if !logged_in?
+            erb :'/users/login'
+        else
+            redirect "/show" #displays a users page
+        end
+    end
+
+    get '/signup' do
         
+    end
+
+    post '/signup' do
+        
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect "/login" 
+        else
+            redirect "/"
+        end
     end
 end
