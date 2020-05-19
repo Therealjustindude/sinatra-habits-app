0e38c109bca3a66eea1960d158dd5421f0bc34b9
therealjustindude <therealjustindude@gmail.com>
2020-05-18 20:36:14 -0500

    update schema and habits migration to say habit in column

--- db/migrate/20200519012437_create_habits_table.rb
+++ db/migrate/20200519012437_create_habits_table.rb
@@ -1,7 +1,7 @@
 class CreateHabitsTable < ActiveRecord::Migration
   def change
     create_table :habits do |t|
-      t.string :habits
+      t.string :habit
       t.integer :user_id
 
       t.timestamps null: false
