9a1b796d517e7cab0c8ea05d9c9355e85b389eed
therealjustindude <therealjustindude@gmail.com>
2020-05-21 17:31:35 -0500

    dynamic routes under static routes

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -9,6 +9,24 @@ class HabitsController < ApplicationController
         end
     end
 
+    get '/habits/new' do
+        if logged_in?
+             erb :'/habits/create_habit'
+        else
+            redirect "/login"
+        end
+    end
+
+    post '/habits' do 
+        if logged_in?
+            @habit = Habit.new(habit: params[:habit],user_id: @current_user.id)
+            @habit.save
+            redirect "/habits"
+        else
+            redirect "/login"
+        end
+    end
+
     get '/habits/:id' do
         @habit = Habit.find_by(id: params[:id])
         if logged_in?
@@ -42,11 +60,11 @@ class HabitsController < ApplicationController
                 @habit.update(habit: params[:habit])
                 redirect "/habits"
             else
+                redirect "user/#{@current_user.id}"
             end
         else
             redirect "/"
         end
     end
-
     
 end
