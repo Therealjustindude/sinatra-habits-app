85685677b02b48154b371da9a52d5778503ce234
therealjustindude <therealjustindude@gmail.com>
2020-05-26 13:24:17 -0500

    updated relationships

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,5 @@
 class Habit < ActiveRecord::Base
     validates :habit, uniqueness: true
-    belongs_to :user
+    has_many :user_habits 
+    has_many :users, through: :user_habits
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
     validates :name, presence: true
     validates :email, presence: true
     validates :email, uniqueness: true
-    has_many :habits
+    has_many :user_habits
+    has_many :habits, through: :user_habits
+
 end
