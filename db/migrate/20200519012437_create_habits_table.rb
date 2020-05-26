b3eb394ee93d2436781b85538530db4648061828
therealjustindude <therealjustindude@gmail.com>
2020-05-26 13:22:45 -0500

    updating habits table

--- db/migrate/20200519012437_create_habits_table.rb
+++ db/migrate/20200519012437_create_habits_table.rb
@@ -2,7 +2,6 @@ class CreateHabitsTable < ActiveRecord::Migration
   def change
     create_table :habits do |t|
       t.string :habit
-      t.integer :user_id
 
       t.timestamps null: false
     end
