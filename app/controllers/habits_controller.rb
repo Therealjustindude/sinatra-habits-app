f172dbc6039f913c80a88aa51d81085a237bf729
therealjustindude <therealjustindude@gmail.com>
2020-05-26 22:25:40 -0500

    create or pick a habit

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -14,35 +14,33 @@ class HabitsController < ApplicationController
 
     post '/habits' do 
         verify_user_logged_in
-        if params[:habit] == "" 
-            flash[:message] = "Please do not leave Habit blank"
-            redirect "/habits" 
-        #add elsif for habit_id find and associate with user
-        else
-            @habit = Habit.new(habit: params[:habit].downcase)
-            if @habit.save
-                @current_user.habits << @habit
-                redirect "/habits"
-            else
-                flash[:message] = "You created this habit already."
-                redirect "/habits" 
-            end
+        @current_user.habits << Habit.find(params[:habit_id]) if params[:habit_id]
+        
+        @habit = Habit.new(habit: params[:habit].downcase) if params[:habit] && params[:habit] != ""
+        
+        if @habit && @habit.save
+            @current_user.habits << @habit  
         end
+        redirect "/habits" 
     end
 
     get '/habits/:id' do
-        if !find_habit
+        verify_user_logged_in
+        if !find_habit 
              flash[:message] = "You have not created this habit yet."
              redirect "/habits" 
-        else
-            verify_user_logged_in
-            verify_habits_permission
-            erb :'/habits/show_habit'   
         end
+        # binding.pry
+        verify_habits_permission 
+
+         erb :'/habits/show_habit' 
     end
 
     get '/habits/:id/edit' do
-        find_habit
+        if !find_habit
+            flash[:message] = "You have not created this habit yet."
+            redirect "/habits" 
+       end
         verify_user_logged_in
         verify_habits_permission
         erb :'habits/edit_habit'
@@ -50,7 +48,6 @@ class HabitsController < ApplicationController
     end
 
     patch '/habits/:id' do
-        find_habit
         verify_user_logged_in
         redirect "users/#{@current_user.id}" if params[:habit] == ""
         @habit.update(habit: params[:habit].downcase)
@@ -58,7 +55,10 @@ class HabitsController < ApplicationController
     end
 
     delete '/habits/:id' do
-        find_habit
+        if !find_habit
+            flash[:message] = "You have not created this habit yet."
+            redirect "/habits" 
+        end
         verify_user_logged_in
         verify_habits_permission  
         @habit.destroy
