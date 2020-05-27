d2ca7b1fe51601b1fe7bbe0b2a1b4e7c0136853f
therealjustindude <therealjustindude@gmail.com>
2020-05-26 22:22:07 -0500

    changed table type to integer

--- db/migrate/20200526180336_user_habits.rb
+++ db/migrate/20200526180336_user_habits.rb
@@ -1,8 +1,8 @@
 class UserHabits < ActiveRecord::Migration
   def change
     create_table :user_habits do |t|
-      t.string :habit_id
-      t.string :user_id
+      t.integer :habit_id
+      t.integer :user_id
       
       t.timestamps null: false
     end
